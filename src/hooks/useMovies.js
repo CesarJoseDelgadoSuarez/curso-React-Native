37a2bd118ecbe08ede73650392728d63abea0f5c
Cesar Delgado <cjdsgithub@gmail.com>
2023-11-12 17:15:11 +0000

    actualizado hook rapidapi

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,18 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { optionsMovie, optionsMovies } from "./rapidApi.config";
 const url = "https://moviesdatabase.p.rapidapi.com/titles";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "2ef8ef4548msh2b3c98b67ab0a14p13e72fjsnc3aae9dd4511",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-  },
-};
 
 const getData = async () => {
   try {
-    const response = await axios.get(url, options);
+    const response = await axios.get(url, optionsMovies);
     return response.data; // Actualiza el estado con la nueva información
   } catch (error) {
     console.error("Error al obtener datos:", error);
@@ -21,14 +14,6 @@ const getData = async () => {
 };
 
 const useGetMovie = async () => {
-  const optionsMovie = {
-    method: "GET",
-    url: "https://moviesdatabase.p.rapidapi.com/titles/tt0000045",
-    headers: {
-      "X-RapidAPI-Key": "26501aa9f6mshbd64b71e8674369p10dcf7jsn4905f35973d3",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  };
   try {
     const response = await axios.request(optionsMovie);
     return response.data.results;
